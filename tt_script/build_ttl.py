2b905f9e3ec3b281c113edb19fb561fb78e56507
孔林 <konglin@aodiansoft.com>
2016-08-24 15:31:46 +0800

    暂存

--- tt_script/build_ttl.py
+++ tt_script/build_ttl.py
@@ -4,31 +4,51 @@ import os
 import json
 
 def main():
-    cwd = lambda f: os.path.join(os.getcwd(), f)
     config = load_json( cwd('server_config.ignore') )
     cfg = fix_config(config);
     for key, item in cfg.items():
-        str_list = dump_ttl(key, item)
+        str_list = connect_ttl(key, item)
         dump_file(cwd(key+'.ttl'), str_list)
+        str_list = clear_log_ttl(key, item)
+        dump_file(cwd('clear_log', key+'_clear.ttl'), str_list)
+
+def cwd(*f):
+    return os.path.join(os.getcwd(), *f)
 
 def dump_file(file_name, str_list):
+    if not os.path.isdir(os.path.dirname(file_name)):
+        os.makedirs(os.path.dirname(file_name))
+
     with open(file_name, 'w') as wf:
         wf.writelines(str_list)
+        print "write file:%s" % (file_name, )
+
+def load_file(file_name):
+    if not os.path.isfile(file_name):
+        return ''
+    with open(file_name, 'r') as rf:
+        return rf.read()
 
 def load_json(file_name):
     with open(file_name, 'r') as rf:
         return json.load(rf)
 
 def fix_config(cfg):
+    base_ini = load_file(cwd('_tt_config_ini', 'TERATERM_TPL.INI'))
     rst = {}
     for key, item in cfg.items():
         item['Prompt'] = '#' if item['User']=='root' else '$'
-        item['Ini'] = 'TERATERM_KL.INI'
-        item['FirstCmd'] = 'ps -ef | grep node' if key.endswith('node') else 'ps -ef | grep "nginx\|httpd"'
+        dump_file(cwd('_tt_config_ini', 'tmp', key+'.INI'), base_ini.replace('{{TPL_TITLE}}', key, 1))
+        item['Ini'] = cwd('_tt_config_ini', 'tmp', key+'.INI')
+        item['FirstCmd'] = 'ps -ef | grep node' if key.find('node')>=0 else 'ps -ef | grep "nginx\|httpd"'
         rst[key] = item
     return rst
 
-def dump_ttl(key, item):
+def _append(str_list, str_append):
+    str_list.append(str_append)
+    return str_list
+
+def base_ttl(key, item):
     return [
         "set_Title ='%s' \n" % (key, ),
         "set_Prompt = '%s' \n" % (item['Prompt'], ),
@@ -49,9 +69,11 @@ strconcat tmp_connect_cmd ' /passwd='
 strconcat tmp_connect_cmd set_Password
 strconcat tmp_connect_cmd ' /f='
 strconcat tmp_connect_cmd set_Ini
+connect tmp_connect_cmd """,
+    ]
 
-connect tmp_connect_cmd
-
+def connect_ttl(key, item):
+    return _append(base_ttl(key, item), """
 ;++++++++++++++++++++++++++++++++++++++++++++
 tmp_cd_cmd = 'cd '
 strconcat tmp_cd_cmd set_WorkPath
@@ -71,9 +93,28 @@ sendln 'git log -1'
 wait set_Prompt
 ;++++++++++++++++++++++++++++++++++++++++++++
 settitle set_Title
-restoresetup set_Ini""",
-    ]
+restoresetup set_Ini
+""")
 
+def clear_log_ttl(key, item):
+    return _append(base_ttl(key, item), """
+;++++++++++++++++++++++++++++++++++++++++++++
+tmp_cd_cmd = 'cd '
+strconcat tmp_cd_cmd set_WorkPath
+
+wait set_Prompt
+sendln tmp_cd_cmd
+wait set_Prompt
+sendln 'pwd'
+wait set_Prompt
+sendln 'rm -rf ./log/*'
+wait set_Prompt
+;++++++++++++++++++++++++++++++++++++++++++++
+settitle set_Title
+restoresetup set_Ini
+disconnect
+closett
+""")
 
 if __name__ == '__main__':
     main()
