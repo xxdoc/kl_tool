a97363be2a9d05799730e77a91fbfc1aea63a6e7
konglin <wuyou7410@gmail.com>
2017-11-07 13:20:18 +0800

    fix

--- test/test_api/access_log.py
+++ test/test_api/access_log.py
@@ -7,6 +7,16 @@ import time
 
 from functools import wraps
 
+import marshal
+
+def dump_obj(obj, file_name):
+    with open(file_name, 'wb') as wf:
+        marshal.dump(obj, wf)
+
+def load_obj(file_name):
+    with open(file_name, 'rb') as rf:
+        return marshal.load(rf)
+        
 def fn_cache(key=None):
     _default_key_func = lambda args, kwargs: '<args:%r,kwargs:%r>' % (args, kwargs)
     _key_func = _default_key_func if key is None else key
@@ -53,7 +63,7 @@ def format_time(timestamp):
     ret = time.strftime('%Y-%m-%d %H:%M:%S', timestruct)
     return ret
 
-def run(url_file, save_seq=10000, interval=300, group_path_func=None):
+def _run(url_file, save_seq, interval, group_path_func):
     '''
        log_format main  '$remote_addr $host [$time_local] "$request" '
                      '$status $request_length $body_bytes_sent "$http_referer" "$http_user_agent" '
@@ -88,42 +98,107 @@ def run(url_file, save_seq=10000, interval=300, group_path_func=None):
             if idx % save_seq == 1:
                 print '.',
 
+    return ret_data, ret_group
+    
+def run(url_file, save_seq=10000, interval=300, group_path_func=None):
+    tmp_ret = url_file + '.ret.tmp'
+    tmp_group = url_file + '.group.tmp'
+    if os.path.isfile(tmp_ret) and os.path.isfile(tmp_ret):
+        ret_data, ret_group = load_obj(tmp_ret), load_obj(tmp_group)
+    else:
+        ret_data, ret_group = _run(url_file, save_seq, interval, group_path_func)
+        dump_obj(ret_data, tmp_ret)
+        dump_obj(ret_group, tmp_group)
+
     ret_data, ret_group = calculateAverage(ret_data), calculateAverage(ret_group)
+    ret_data, ret_group = listIntervalMap(ret_data), listIntervalMap(ret_group)
     return ret_data, ret_group
 
-def update_count_item(obj, key, request_time, request_length, body_bytes_sent, status, timer_count):
-    obj.setdefault(key, {
+def listIntervalMap(ret_data):
+    key = 'interval_map'
+    for url, item in ret_data.iteritems():
+        data_map = item.get(key, {})
+        s_func = lambda l: sorted(l, key=lambda t: t[0])
+        f_func = lambda k: '2017-11-06 20:00:00' <= k <= '2017-11-06 22:00:00'
+        r_func = lambda k, v: ( "%s#%s" % (k, v) ).replace(':00#', '#').split(' ', 1)[-1]
+        ret_data[url][key] = fix_ret_map(data_map, s_func, r_func, f_func, '\n')
+
+    key = 'status_map'
+    for url, item in ret_data.iteritems():
+        data_map = item.get(key, {})
+        s_func = lambda l: sorted(l, key=lambda t: t[0])
+        f_func = lambda k: k
+        r_func = lambda k, v: "%s#%s" % (k, v)
+        ret_data[url][key] = fix_ret_map(data_map, s_func, r_func, f_func, '\n')
+        
+    return ret_data
+
+def fix_ret_map(data_map, s_func, r_func, f_func, seq=','):
+    item_list = s_func(data_map.iteritems())
+
+    ret_list = []
+    for k, v in item_list:
+        if f_func(k):
+            tmp_str = r_func(k, v)
+            ret_list.append(tmp_str)
+    return seq.join(ret_list)
+    
+def update_count_item(obj, url, request_time, request_length, body_bytes_sent, status, timer_count):
+    item = obj.get(url, {
         'num': 0,
         'status_map': {},
         'interval_map': {},
         'total_request_time': 0.0,
         'total_request_length': 0.0,
         'total_body_bytes_sent': 0.0,
+        'max_request_time': 0.0,
+        'max_request_length': 0.0,
+        'max_body_bytes_sent': 0.0,
+        'min_request_time': 0.0,
+        'min_request_length': 0.0,
+        'min_body_bytes_sent': 0.0,
     })
+    request_time, request_length, body_bytes_sent = float(request_time), float(request_length), float(body_bytes_sent)
+    item['num'] += 1
+    item['total_request_time'] += request_time
+    item['total_request_length'] += request_length
+    item['total_body_bytes_sent'] += body_bytes_sent
+    if status and (int(status)==200 or int(status)==201 or int(status)==206):
+        item['max_request_time'] = request_time if request_time > item['max_request_time'] else item['max_request_time']
+        item['max_request_length'] = request_length if request_length > item['max_request_length'] else item['max_request_length']
+        item['max_body_bytes_sent'] = body_bytes_sent if body_bytes_sent > item['max_body_bytes_sent'] else item['max_body_bytes_sent']
+        item['min_request_time'] = request_time if request_time < item['min_request_time'] else item['min_request_time']
+        item['min_request_length'] = request_length if request_length < item['min_request_length'] else item['min_request_length']
+        item['min_body_bytes_sent'] = body_bytes_sent if body_bytes_sent < item['min_body_bytes_sent'] else item['min_body_bytes_sent']
+    
+    item['status_map'][ status ] = item['status_map'].get(status, 0) + 1
+    item['interval_map'][ timer_count ] = item['interval_map'].get(timer_count, 0) + 1
 
-    obj[ key ]['num'] += 1
-    obj[ key ]['total_request_time'] += float(request_time)
-    obj[ key ]['total_request_length'] += float(request_length)
-    obj[ key ]['total_body_bytes_sent'] += float(body_bytes_sent)
-    obj[ key ]['status_map'][ status ] = obj[ key ]['status_map'].get(status, 0) + 1
-    obj[ key ]['interval_map'][ timer_count ] = obj[ key ]['interval_map'].get(timer_count, 0) + 1
+    obj[ url ] = item
 
 def calculateAverage(obj):
     for key, val in obj.iteritems():
-        val['average_request_time'] = round(val['total_request_time'] / val['num'], 2)
-        val['average_request_length'] = round(val['total_request_length'] / val['num'], 2)
-        val['average_body_bytes_sent'] = round(val['total_body_bytes_sent'] / val['num'], 2)
+        val['ave_request_time'] = round(val['total_request_time'] / val['num'], 2)
+        val['ave_request_length'] = round(val['total_request_length'] / val['num'], 2)
+        val['ave_body_bytes_sent'] = round(val['total_body_bytes_sent'] / val['num'], 2)
 
     return obj
 
 def writeCsv(csv_file, obj):
+    s_map = {'request':'req', 'body_bytes':'body'}
+    def _fix(str_k, replace_map):
+        for k, v in replace_map.items():
+            str_k = str_k.replace(k, v)
+        return str_k
+        
     with open(csv_file, 'wb') as csvfile:
         fieldnames = ['url', ] + obj[ obj.keys()[0] ].keys()
+        fieldnames = [_fix(i, s_map) for i in fieldnames]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for url, item in obj.iteritems():
-            item['url'] = url
-            writer.writerow(item)
+            item['url'] = url[:50]
+            writer.writerow({_fix(k, s_map): v for k, v in item.items()})
 
 def main():
     print '\n---------------Start-------------------\n'
